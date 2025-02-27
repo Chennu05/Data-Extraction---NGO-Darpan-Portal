7ac52a9f3014ccca786ad5c2b402293583459998
Chennu05 <120401809+Chennu05@users.noreply.github.com>
2025-02-28 02:32:05 +0530

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,7 @@
+# config.py
+
+BASE_URL = "https://ngodarpan.gov.in/index.php/home/statewise_ngo"
+HEADERS = {
+    "User-Agent": "Mozilla/5.0"
+}
+OUTPUT_FORMAT = "csv"  # Change to "json" for JSON output
